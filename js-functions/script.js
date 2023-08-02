eef83e565a5e96283b0d91e4143704bfeede0dc8
Aniela Fendrych <aniela.fendrych@gmail.com>
2023-08-02 22:25:01 +0200

    bubble-sort-algorithm

--- js-functions/script.js
+++ js-functions/script.js
@@ -95,3 +95,21 @@ function iloscPrzeczytanych(array) {
 
 //iloscPrzeczytanych(listOfBooks);
 //************************************** */
+
+function bubbleSort(arr) {
+    let swap;
+    do {
+        swap = false;
+        for ( let i = 0; i < arr.length; i++ ) {
+            if ( arr[i] > arr[i + 1] ) {
+                [myArray[i], myArray[i + 1]] = [myArray[i + 1], myArray[i]];
+                swap = true;
+            }
+        }
+    } while(swap);
+    return arr;
+}
+
+//let myArray =  [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
+//console.log(bubbleSort(myArray));
+//************************************** */
