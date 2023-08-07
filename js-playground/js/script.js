8f63b2bd05442461acf744ac2f1e5c88153602f6
Aniela Fendrych <aniela.fendrych@gmail.com>
2023-08-07 16:04:57 +0200

    3-ustaw-tlo-elementow-html

--- js-playground/js/script.js
+++ js-playground/js/script.js
@@ -1,1 +1,13 @@
 //Initial
+
+const setBackground = () => {
+    let p1 = document.querySelector(".first");
+    let p2 = document.querySelector(".last");
+
+    //p1.style.backgroundcolor = 'red'; // .style.backgroundcolor is not working anymore
+    p1.classList.add('bg-red'); //.toggle is also nice instead of .add
+    p2.classList.add('bg-yellow'); //.toggle
+}
+
+let btnSetBackground = document.getElementById('set-background');
+btnSetBackground.addEventListener('click', setBackground);
