8afd756349c0f94f4662138430d384138fe87474
Aniela Fendrych <aniela.fendrych@gmail.com>
2023-08-07 12:06:56 +0200

    concept-of-nodes
    .appendChild

--- js-dom/js/script.js
+++ js-dom/js/script.js
@@ -1,24 +1,40 @@
-//model DOM, obiekt window
+//concept of nodes
 
-//getElementById - 1 element:
+let node = document;
+
+// Node.nodeType == ELEMENT_NODE
+node = document.documentElement;
+node = document.head;
+node = document.body;
+
+// Node.nodeType == ATTRIBUTE_NODE
+console.log(document.getElementById('articles').childNodes);
+node = document.getElementById('articles').childNodes[3].attributes[0];
+console.log(node); //wyswietla id
+node = document.getElementById('articles').childNodes[3].attributes[1];
+console.log(node); //wyswietla klasę
+
+// Node.nodeType == TEXT_NODE
+console.log(document.getElementsByClassName('content'));
+console.log(document.getElementsByClassName('content')[0].childNodes);
+console.log(document.getElementsByClassName('content')[0].childNodes[0]);
+
+// Node.nodeType == COMMENT_NODE
+console.log(document.getElementById('articles').childNodes);
+console.log(document.getElementById('articles').childNodes[1]);
+
+// Node.nodeType == DOCUMENT_NODE
+node = document;
+
+// Node.nodeType == DOCUMENT_TYPE_NODE
+console.log(document.doctype);
+
+// Node.nodeType == DOCUMENT_FRAGMENT_NODE
+let documentFragment = new DocumentFragment();
+node = documentFragment;
 let articles = document.getElementById('articles');
-console.log(articles);
-
-//querySelector - 1 element; pierwszy z wszystkich pasujących elementów:
-let paragraph = document.querySelector('.content');
-console.log(paragraph);
-
-//HTML-collection; podobny do tablic:
-
-//getElementsByClassName:
-let paragraphs = document.getElementsByClassName('content');
-console.log(paragraphs);
-console.log(paragraphs[1]);
-
-//getElementsByTagName:
-let allParagraphs = document.getElementsByTagName('p');
-console.log(allParagraphs);
-
-//querySelectorAll - format elementu NODELIST
-let parContent = document.querySelectorAll('p.content');
-console.log(parContent);
+let article2 = document.createElement('article');
+article2.innerText = 'Lorem ipsum tralalala';
+//appendChild
+documentFragment.appendChild(article2);
+articles.appendChild(documentFragment);
