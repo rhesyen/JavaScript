93a0f0ca4e5de7e78fc2e5d09a2ff8c62ff1a8d2
Aniela Fendrych <aniela.fendrych@gmail.com>
2023-08-11 18:50:31 +0200

    4-obsluga-formularza-imie-i-nazwisko
    with DOM

---
+++ js-playground2/js/script.js
@@ -0,0 +1,12 @@
+let form = document.getElementById('form');
+
+const submitForm = (event) => {
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name="lname"]');
+
+    console.log(`Imie: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+form.addEventListener('submit', submitForm);
